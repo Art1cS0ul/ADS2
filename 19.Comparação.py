b307dc0746f19a6fde827d13c735fc86a19a88bb
JS <77242684+Art1cS0ul@users.noreply.github.com>
2026-09-17 15:53:41 -0300

    Comparação de valores
    Recebe 2 números reais e mostra o maior entre eles

---
+++ 19.Comparação.py
@@ -0,0 +1,8 @@
+V1=float(input("Digite o primeiro valor: "))
+V2=float(input("Digite o segundo valor: "))
+if V1>V2:
+    print("O maior valor é" , V1)
+elif V1==V2:
+    print("Os valores são iguais")
+else:
+    print("O maior valor é:" , V2)
